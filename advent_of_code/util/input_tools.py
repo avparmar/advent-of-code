a603d8e7b6fb68a3cdbd19a076dec12f78585358
Adi Parmar <adiadi@google.com>
2024-12-01 17:47:57 -0500

    nonon

--- advent_of_code/util/input_tools.py
+++ advent_of_code/util/input_tools.py
@@ -1,5 +1,3 @@
-AOC_TOKEN = '53616c7465645f5f68a7ad38ddc91ce363d0c8107a5ceb9c543568bb03cffd622f17f14f8adb170b38cc6cf57be6decb00ab5ec1c81fbd491fe295b8e0affad9'
-
 def get_input_rows(filename = "input.txt"):
     with open('input.txt') as input:
         rows = [x for x in input.read().strip().split("\n")]
